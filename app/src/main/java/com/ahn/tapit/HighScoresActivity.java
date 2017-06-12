ad888384f6f5c277b35fe07391b84b4448517c2d
AliNaqvi97 <alinaqvi1397@gmail.com>
2017-06-11 22:44:10 -0500

    Update Highscores.java and rename to HighScoresActivity.java

---
+++ app/src/main/java/com/ahn/tapit/HighScoresActivity.java
@@ -0,0 +1,56 @@
+package com.ahn.tapit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HighScoresActivity extends AppCompatActivity {
+    @BindView(R.id.button_main_menu)
+    Button buttonMainMenu;
+    @BindView(R.id.button_play_again)
+    Button buttonPlayAgain;
+    @BindView(R.id.score_listview)
+    ListView scoreListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscores);
+        ButterKnife.bind(this);
+
+        ArrayList<Integer> scores = new DatabaseHandler(HighScoresActivity.this).getAllScores();
+
+        if (scores.size() < 10) {
+            while (0 < 10 - scores.size()) {
+                scores.add(null);
+            }
+        }
+
+        ListViewAdapter adapter = new ListViewAdapter(HighScoresActivity.this,
+                R.layout.score_list, scores);
+        scoreListView.setAdapter(adapter);
+
+        buttonMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        buttonPlayAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HighScoresActivity.this, Game.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/ahn/tapit/MainActivity.java
+++ app/src/main/java/com/ahn/tapit/MainActivity.java
@@ -6,15 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.ArrayList;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.button_play) Button buttonPlay;
-    @BindView(R.id.button_instructions) Button buttonInstructions;
-    @BindView(R.id.button_high_scores) Button buttonHighScores;
+    @BindView(R.id.button_play)
+    Button buttonPlay;
+    @BindView(R.id.button_instructions)
+    Button buttonInstructions;
+    @BindView(R.id.button_high_scores)
+    Button buttonHighScores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +37,7 @@ public class MainActivity extends AppCompatActivity {
         buttonHighScores.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Highscores.class);
-                ArrayList<Integer> scores = new DatabaseHandler(MainActivity.this).getAllScores();
-                int x = 0;
-                if (scores.size() < 10) {
-                    while (x < 10 - scores.size()) {
-                        scores.add(null);
-                    }
-                }
-                intent.putIntegerArrayListExtra("data", scores);
-                startActivity(intent);
+                startActivity(new Intent(MainActivity.this, HighScoresActivity.class));
             }
         });
     }
