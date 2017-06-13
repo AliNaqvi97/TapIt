3f578e82f988addc361f05956a6b8718b6ceed0d
AliNaqvi97 <alinaqvi1397@gmail.com>
2017-06-13 17:43:49 -0500

    Update dialog and rename to PauseDialog.java

---
+++ app/src/main/java/com/ahn/tapit/PauseDialog.java
@@ -0,0 +1,72 @@
+package com.ahn.tapit;
+
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MyDialog extends DialogFragment {
+    @BindView(R.id.resume_button)
+    private Button resumeButton;
+    @BindView(R.id.restart_button)
+    private Button restartButton;
+    @BindView(R.id.highscore_button)
+    private Button highScoreButton;
+    @BindView(R.id.quit_button)
+    private Button quitButton;
+    private Game gameObject;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = View.inflate(getActivity(), R.layout.pause_dialogue, null);
+        ButterKnife.bind(this, view);
+
+        resumeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+                gameObject.resumeGame(false);
+            }
+        });
+
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+                gameObject.resumeGame(true);
+            }
+        });
+
+        highScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+                startActivity(new Intent(getActivity(), HighScoresActivity.class));
+                getActivity().finish();
+            }
+        });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+                getActivity().finish();
+            }
+        });
+
+        return view;
+    }
+
+    public void setObject(Game object){
+        gameObject = object;
+    }
+}
